89dcceb7de9b5c33f56bec4323ba4da2b4ab10df
hnmx4 <haneuma0628@gmail.com>
2016-05-02 05:15:28 +0900

    get weekly contributions.

--- get_contributions.py
+++ get_contributions.py
@@ -5,7 +5,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
-def pick_count_list(res):
+def pick_count_w_date_list(res):
   lists = []
   dict_obj = {}
   html = res.read()
@@ -19,8 +19,21 @@ def pick_count_list(res):
   return lists
 
 
-url = 'https://github.com/users/hnmx4/contributions'
-res = urllib.request.urlopen(url)
+def pick_count_list(res):
+  lists = []
+  html = res.read()
+  soup = BeautifulSoup(html, 'html.parser')
+  for rect in soup.find_all('rect'):
+    lists.append(rect['data-count'])
+  return lists
 
-lists = pick_count_list(res)
-print(lists)
+
+url = 'https://github.com/users/hnmx4/contributions'
+lists = pick_count_list(urllib.request.urlopen(url))
+weekly = lists[-7:]
+
+weekly_contributions = 0
+for day in weekly:
+  weekly_contributions += int(day)
+
+print(weekly_contributions)
