f2137a4dbe4bd76481cb3cdb46342321d2573692
hnmx4 <haneuma0628@gmail.com>
2016-05-24 13:43:59 +0900

    show weekly contributions with 'emoji'. and, answer to a message that if contain a particular word.

--- notify_lawn.py
+++ notify_lawn.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import time
 import urllib2
 import get_contributions as gc
@@ -11,14 +13,40 @@ dotenv_path = join(dirname(__file__), '.env')
 crontable = []
 outputs = []
 
-crontable.append([86400, 'say_count_contributions'])
+CHANNEL_ID = get_key(dotenv_path, 'CHANNEL_ID')
+lawns = { 0:':new_moon_with_face:',
+          1:':seedling:',
+          2:':herb:',
+          3:':deciduous_tree:',
+          4:':cherry_blossom:'}
 
-def say_count_contributions():
-    lists = gc.pick_count_list(urllib2.urlopen(urllib2.Request(get_key(dotenv_path, 'URL'))))
+
+def pick_weekly_contributions():
+    url = 'https://github.com/users/' + get_key(dotenv_path, 'USER') + '/contributions'
+    lists = gc.pick_count_list(urllib2.urlopen(urllib2.Request(url)))
     weekly = lists[-7:]
 
     weekly_contributions = 0
     for day in weekly:
         weekly_contributions += int(day)
 
-    outputs.append([get_key(dotenv_path, 'CHANNEL_ID'), "weekly contributions: " + str(weekly_contributions)])
+    return weekly_contributions
+
+
+def say_weekly_contributions():
+    outputs.append([CHANNEL_ID, lawns[pick_weekly_contributions()]])
+
+
+def process_message(data):
+    words = ['今週'.decode('utf-8'), '草'.decode('utf-8')]
+    if data['channel'] == CHANNEL_ID:
+        flag = True
+        for word in words:
+            if data['text'].find(word) == -1:
+                flag = False
+
+        if (flag):
+            outputs.append([CHANNEL_ID, lawns[pick_weekly_contributions()]])
+
+
+say_weekly_contributions()
