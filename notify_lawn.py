4a361d1399bb5010b95a5fee72dc86ced218a32e
hnmx4 <haneuma0628@gmail.com>
2016-05-29 10:08:58 +0900

    change way that show contributions.
    * 'emoji' shows contributions of yesterday.
    
    * number of emoji shows contributions of week.

--- notify_lawn.py
+++ notify_lawn.py
@@ -14,39 +14,48 @@ crontable = []
 outputs = []
 
 CHANNEL_ID = get_key(dotenv_path, 'CHANNEL_ID')
-lawns = { 0:':new_moon_with_face:',
-          1:':seedling:',
-          2:':herb:',
-          3:':deciduous_tree:',
-          4:':cherry_blossom:'}
+def lawns(con):
+    if con < 5:
+        e_lists = { 0:':fallen_leaf:',
+                    1:':seedling:',
+                    2:':herb:',
+                    3:':deciduous_tree:',
+                    4:':cherry_blossom:'}
+        return e_lists[con]
+    else:
+        return ':cherry_blossom:'
 
 
-def pick_weekly_contributions():
+def say_contributions():
     url = 'https://github.com/users/' + get_key(dotenv_path, 'USER') + '/contributions'
     lists = gc.pick_count_list(urllib2.urlopen(urllib2.Request(url)))
     weekly = lists[-7:]
+    yesterday = int(lists[-1])
 
-    weekly_contributions = 0
+    wc = 0
     for day in weekly:
-        weekly_contributions += int(day)
+        wc += int(day)
 
-    return weekly_contributions
+    mesg = ''
+    if wc > 0:
+        for i in range(0, wc):
+            mesg += lawns(yesterday)
+    else:
+        mesg = ':new_moon_with_face:'
 
-
-def say_weekly_contributions():
-    outputs.append([CHANNEL_ID, lawns[pick_weekly_contributions()]])
-
-
-def process_message(data):
-    words = ['今週'.decode('utf-8'), '草'.decode('utf-8')]
-    if data['channel'] == CHANNEL_ID:
-        flag = True
-        for word in words:
-            if data['text'].find(word) == -1:
-                flag = False
-
-        if (flag):
-            outputs.append([CHANNEL_ID, lawns[pick_weekly_contributions()]])
+    outputs.append([CHANNEL_ID, mesg])
 
 
-say_weekly_contributions()
+# def process_message(data):
+#     words = ['今週'.decode('utf-8'), '草'.decode('utf-8')]
+#     if data['channel'] == CHANNEL_ID:
+#         flag = True
+#         for word in words:
+#             if data['text'].find(word) == -1:
+#                 flag = False
+
+#         if (flag):
+#             outputs.append([CHANNEL_ID, lawns[pick_weekly_contributions()]])
+
+
+say_contributions()
