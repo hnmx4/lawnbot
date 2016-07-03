6105f266830a2c42a7a6f4ebd043c4c765fc8fca
hnmx4 <haneuma0628@gmail.com>
2016-07-03 10:21:00 +0900

    merge tpick_dayly_count to notify_lawn.py

--- notify_lawn.py
+++ notify_lawn.py
@@ -1,27 +1,48 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-import urllib2
 from dotenv import get_key
+import urllib2
 from os.path import join, dirname
-
-import get_contributions as gc
-
+from bs4 import BeautifulSoup
 
 def denv(key):
     return get_key(join(dirname(__file__), '.env'), key)
+
 
 crontable = []
 outputs = []
 
 CHANNEL_ID = denv('CHANNEL_ID')
+
+
+def pick_dayly_count(url, opt='dict'):
+    res = urllib2.urlopen(urllib2.Request(url))
+    data = {}
+    counts = []
+    html = res.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    for rect in soup.find_all('rect'):
+        if opt == 'dict':
+            data[rect['data-date']] = rect['data-count']
+        elif opt == 'list':
+            counts.append(rect['data-count'])
+
+    if opt == 'dict':
+        return data
+    elif opt == 'list':
+        return counts
+
+
 def lawns(con):
     if con < 5:
-        e_lists = { 0:':fallen_leaf:',
-                    1:':seedling:',
-                    2:':herb:',
-                    3:':deciduous_tree:',
-                    4:':cherry_blossom:'}
+        e_lists = {
+            0: ':fallen_leaf:',
+            1: ':seedling:',
+            2: ':herb:',
+            3: ':deciduous_tree:',
+            4: ':cherry_blossom:'
+        }
         return e_lists[con]
     else:
         return ':cherry_blossom:'
@@ -29,8 +50,8 @@ def lawns(con):
 
 def say_contributions():
     url = 'https://github.com/users/' + denv('USER') + '/contributions'
-    data = gc.pick_dayly_count(url)
-    counts = gc.pick_dayly_count(url, 'list')
+    data = pick_dayly_count(url)
+    counts = pick_dayly_count(url, 'list')
     yday = datetime.date.today() - datetime.timedelta(1)
     yday = yday.strftime('%Y-%m-%d')
     yc = data[yday]
