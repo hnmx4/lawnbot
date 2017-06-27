dd1741e1f2e6c19d4f73179d89ce4ee7dfb133b6
hnmx4 <hnmnty@gmail.com>
2017-06-28 01:30:00 +0900

    update according to python-rtmbot0.4.0. #4

--- notify_lawn.py
+++ notify_lawn.py
@@ -1,87 +1,87 @@
 # -*- coding: utf-8 -*-
 import datetime
-import urllib2
+import urllib3
+import certifi
 import yaml
 
-from dotenv import get_key
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+from os import environ
 from os.path import join, dirname
+from rtmbot.core import Plugin
 
 
-def denv(key):
-    return get_key(join(dirname(__file__), '.env'), key)
+class Notification(Plugin):
+    load_dotenv(join(dirname(__file__), '.env'))
 
+    def configure(self):
+        f = open(join(dirname(__file__), 'conf.yml'), 'r')
+        config = yaml.load(f)
+        f.close()
+        return config
 
-CHANNEL_ID = denv('CHANNEL_ID')
-USER = denv('USER')
-CONF_FILE = join(dirname(__file__), 'conf.yml')
+    def scrap(self, username):
+        url = 'https://github.com/users/' + username + '/contributions'
+        http = urllib3.PoolManager(
+            cert_reqs='CERT_REQUIRED',  # Force certificate check.
+            ca_certs=certifi.where(),  # Path to the Certifi bundle.
+        )
+        r = http.request('GET', url)
+        html = r.data
+        return BeautifulSoup(html, 'html.parser')
 
-f = open(CONF_FILE, 'r')
-config = yaml.load(f)
-f.close
+    def pick_dayly_level(self, username, opt='dict'):
+        config = self.configure()
+        level = config['level']
 
-emoji = config['emoji']
-level = config['level']
-crontable = []
-outputs = []
+        data = {}
+        counts = []
+        soup = self.scrap(username)
+        for rect in soup.find_all('rect'):
+            if opt == 'dict':
+                data[rect['data-date']] = level[rect['fill']]
+            elif opt == 'list':
+                counts.append(level[rect['fill']])
 
-
-def scrap(username):
-    url = 'https://github.com/users/' + username + '/contributions'
-    html = urllib2.urlopen(urllib2.Request(url)).read()
-    return BeautifulSoup(html, 'html.parser')
-
-
-def pick_dayly_level(username, opt='dict'):
-    data = {}
-    counts = []
-    soup = scrap(username)
-    for rect in soup.find_all('rect'):
         if opt == 'dict':
-            data[rect['data-date']] = level[rect['fill']]
+            return data
         elif opt == 'list':
-            counts.append(level[rect['fill']])
+            return counts
 
-    if opt == 'dict':
-        return data
-    elif opt == 'list':
-        return counts
+    def pick_dayly_count(self, username, opt='dict'):
+        data = {}
+        counts = []
+        soup = self.scrap(username)
+        for rect in soup.find_all('rect'):
+            if opt == 'dict':
+                data[rect['data-date']] = rect['data-count']
+            elif opt == 'list':
+                counts.append(rect['data-count'])
 
-
-def pick_dayly_count(username, opt='dict'):
-    data = {}
-    counts = []
-    soup = scrap(username)
-    for rect in soup.find_all('rect'):
         if opt == 'dict':
-            data[rect['data-date']] = rect['data-count']
+            return data
         elif opt == 'list':
-            counts.append(rect['data-count'])
+            return counts
 
-    if opt == 'dict':
-        return data
-    elif opt == 'list':
-        return counts
+    def process_message(self, data):
+        config = self.configure()
+        emoji = config['emoji']
+        user = environ.get('USER')
 
+        level = self.pick_dayly_level(user)
+        counts = self.pick_dayly_count(user, 'list')
+        yday = datetime.date.today() - datetime.timedelta(1)
+        yday = yday.strftime('%Y-%m-%d')
+        yc = int(level[yday])
+        wc = 0
+        for day in counts[-7:]:
+            wc += int(day)
 
-def say_contributions():
-    level = pick_dayly_level(USER)
-    counts = pick_dayly_count(USER, 'list')
-    yday = datetime.date.today() - datetime.timedelta(1)
-    yday = yday.strftime('%Y-%m-%d')
-    yc = int(level[yday])
-    wc = 0
-    for day in counts[-7:]:
-        wc += int(day)
+        mesg = ''
+        if wc > 0:
+            for i in range(0, wc):
+                mesg += emoji[yc]
+        else:
+            mesg = ':new_moon_with_face:'
 
-    mesg = ''
-    if wc > 0:
-        for i in range(0, wc):
-            mesg += emoji[yc]
-    else:
-        mesg = ':new_moon_with_face:'
-
-    outputs.append([CHANNEL_ID, mesg])
-
-
-say_contributions()
+        self.outputs.append([environ.get('CHANNEL_ID'), mesg])
